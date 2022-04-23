f3b0c143b6ba396d05ccc1791f1cf4ca1b58cb86
Tun23 <alakay12305@gmail.com>
2022-04-23 09:24:47 +0700

    fix for run vite

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import _axios from 'axios'
 
-_axios.defaults.baseURL = process.env.VUE_APP_API_URL
+_axios.defaults.baseURL = import.meta.env.VITE_API_URL
 const axios = _axios.create()
 export { axios }
 export default {
